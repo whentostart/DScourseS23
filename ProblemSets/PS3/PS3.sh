bbef4a3da94b81cb17833f18816a8371cc91a2bc
Econ Account <ouecon005@schooner2.oscer.ou.edu>
2023-02-13 22:39:26 -0600

    Added untracked files

---
+++ ProblemSets/PS3/PS3.sh
@@ -0,0 +1,13 @@
+# Step (g)
+wc -l FL_insurance_sample.csv
+
+# Step (h)
+dos2unix -c mac FL_insurance_sample.csv
+
+# Step (i)
+head FL_insurance_sample.csv
+
+# Step (j)
+wc -l FL_insurance_sample.csv
+
+
